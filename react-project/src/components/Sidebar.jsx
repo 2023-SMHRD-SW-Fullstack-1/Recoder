b477bd45397569e11d8ac48b2a68150579cf9a4f
shinyoungkim30 <ksyq12@gmail.com>
2023-09-04 18:09:51 +0900

    commit

---
+++ react-project/src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div style={{
+      width: '200px',
+      height: '100vh',
+      backgroundColor: 'white'
+    }}>
+      Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
