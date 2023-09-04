4ce413e1f579a4e78e72188cef6230cc3ca46d96
임혜주 <lakm1225@gmail.com>
2023-09-04 11:59:40 +0900

    out_01

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import out_01 from "./components/out_01";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <out_01/>
     </div>
-  );
+  )
 }
+
 
 export default App;
---
+++ react-project/src/components/out_01.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function out_01() {
+  return (
+ 
+           <div id='out_01'>
+<div id='out_01_top'>
+<button>출고등록</button>
+<button>출고이력</button>
+<button>배송지관리</button>
+</div>
+<div id='out_01_bottom'>
+<table>
+    <td>
+        <tr>체크박스</tr>
+        <tr>제품ID</tr>
+        <tr>제품코드</tr>
+        <tr>제품명</tr>
+        <tr>용량</tr>
+        <tr>수량</tr>
+        <tr>입고일</tr>
+        <tr>유통기한</tr>
+        <tr>적재위치</tr>
+    </td>
+</table>
+</div>
+
+    </div>
+  )
+}
+
+export default out_01
