ccd846a4c490e5f7c9501bd5183a2c6c7ba830b9
shinyoungkim30 <ksyq12@gmail.com>
2023-09-07 16:03:17 +0900

    env, config.js 생성

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ const Notice = require('./notice')
 const Stock = require('./stock')
 
 const env = process.env.NODE_ENV || 'development'
-const config = require('../config/config')[env]
+const config = require('../config/config.js')[env]
 const db = {}
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
