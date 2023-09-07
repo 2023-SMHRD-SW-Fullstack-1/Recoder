eb45a558a0edde82401bc06f37f536014b758dd6
임혜주 <lakm1225@gmail.com>
2023-09-07 18:02:02 +0900

    db_router
    aa

--- routes/out.js
+++ routes/out.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const db_config = require('../config/config')
-
-const conn = db_config.init()
-db_config.connect(conn)
+const User = require('../models/user');
 
 
 // 출고 메인 페이지 => 출고될 리스트 조회
 router.post('/main',(req,res)=>{
 
-    
+
 
 })
 
