05a3c7deb150219f0ada4b12abc495b5c84417dd
김수연 <playskal@naver.com>
2023-09-11 09:27:36 +0900

    230911 09:27

--- react-project/src/components/In.jsx
+++ react-project/src/components/In.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import '../css/In.css'
 // 폰트어썸
@@ -12,6 +12,16 @@ const In = () => {
 		// 테이블 클릭 ~
 	}
 	console.log(new Date())
+
+	window.addEventListener('resize', e => {
+		// console.log("브라우저 가로", window.innerWidth)
+		// console.log(e)
+
+		if(window.innerWidth < 600) {
+			window.resizeTo(1200, window.innerHeight)
+			console.log("11")
+		}
+	})
 
 	return (
 		<div id='in_all'>
@@ -50,60 +60,113 @@ const In = () => {
 				</table>
 			</div>
 
-
-			<table className="table-none">
-				<thead>
-					<tr>
-						<th>바코드</th>
-						<th>품복코드</th>
-						<th>대표이미지</th>
-						<th>상품명</th>
-						<th>옵션</th>
-						<th>로케이션</th>
-						<th>공급사명</th>
-						<th>현재재고</th>
-						<th>입고대기</th>
-						<th>입고수량</th>
-						<th>입고예정수량</th>
-						<th>비고</th>
-						<th>관리</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>AAAAAABDDO</td>
-						<td>P0000BCH000D</td>
-						<td><img src='https://images.unsplash.com/photo-1610972221114-c48c6bb5d2eb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'/></td>
-						<td>튤립라인 원피스</td>
-						<td>화이트/M</td>
-						<td> - </td>
-						<td>자체공급</td>
-						<td>11</td>
-						<td>0</td>
-						<td>1</td>
-						<td><input type='text' /></td>
-						<td><input type='text' /></td>
-						<td><button type='button'>입고</button></td>
-					</tr>
-					<tr>
-						<td>AAAAAABDD4</td>
-						<td>P0000BCH000C</td>
-						<td><img src='https://images.unsplash.com/photo-1591710668263-bee1e9db2a26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1307&q=80'/></td>
-						<td>튤립라인 원피스</td>
-						<td>화이트/S</td>
-						<td> - </td>
-						<td>자체공급</td>
-						<td>10</td>
-						<td>0</td>
-						<td>0</td>
-						<td><input type='text' /></td>
-						<td><input type='text' /></td>
-						<td><button type='button'>입고</button></td>
-					</tr>
-				</tbody>
-			</table>
+			<div className="table-none">
+				<table>
+					<thead>
+						<tr>
+							<th>바코드</th>
+							<th>품복코드</th>
+							<th>대표이미지</th>
+							<th>상품명</th>
+							<th>옵션</th>
+							<th>로케이션</th>
+							<th>공급사명</th>
+							<th>현재재고</th>
+							<th>입고대기</th>
+							<th>입고수량</th>
+							<th>입고예정수량</th>
+							<th>비고</th>
+							<th>관리</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>AAAAAABDDO</td>
+							<td>P0000BCH000D</td>
+							<td><img src='https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1976&q=80'/></td>
+							<td>튤립라인 원피스</td>
+							<td>화이트/M</td>
+							<td> - </td>
+							<td>자체공급</td>
+							<td>11</td>
+							<td>0</td>
+							<td>1</td>
+							<td><input type='text' /></td>
+							<td><input type='text' /></td>
+							<td><button type='button'>입고</button></td>
+						</tr>
+						<tr>
+							<td>AAAAAABDD4</td>
+							<td>P0000BCH000C</td>
+							<td><img src='https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1976&q=80'/></td>
+							<td>튤립라인 원피스</td>
+							<td>화이트/S</td>
+							<td> - </td>
+							<td>자체공급</td>
+							<td>10</td>
+							<td>0</td>
+							<td>0</td>
+							<td><input type='text' /></td>
+							<td><input type='text' /></td>
+							<td><button type='button'>입고</button></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 			{/* <img src='https://r77.cooltext.com/rendered/cooltext442599808940000.png' width={"30%"} style={{margin: "auto"}}></img> */}
-			
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>dd</div>
+			<div>88</div>
+			<div>99</div>
 			
 		</div>  // div끝
 	)
