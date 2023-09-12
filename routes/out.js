2270822737d5ad94d76cc4227a162f8790a384a9
임혜주 <lakm1225@gmail.com>
2023-09-12 10:07:53 +0900

    sequelize_update

--- routes/out.js
+++ routes/out.js
@@ -2,16 +2,15 @@ const express = require('express')
 const router = express.Router()
 // const User = require('../models/user');
 const { User, Warehouse, Rack, Loading, Stock, Company } = require('../models'); // 모델들을 import
-
-
+const { Op } = require('sequelize');
 
 
 // 출고 메인 페이지 => 출고될 리스트 조회
 router.post('/create', async (req, res) => {
-    let { id } = req.body;
+    let id = req.body.id
+
     try {
-        const outList = await User.findAll({
-            attributes: ['com_seq'],
+        const result = await User.findAll({
             where: {
                 user_id: id
             },
@@ -19,64 +18,60 @@ router.post('/create', async (req, res) => {
                 model: Company,
                 include: [{
                     model: Warehouse,
-                    attributes: ['wh_seq', 'wh_name']
-                    ,
+                    attributes: ['wh_seq', 'wh_name'],
                     include: [{
                         model: Rack,
-
                         include: [{
                             model: Loading,
-                          
+                            where: {
+                                loading_type: { [Op.ne]: 'O' }
+                            },
                             include: [{
-                                model: Stock,
-
+                                model: Stock
                             }]
                         }]
                     }]
                 }]
-
             }]
-        });
-        console.log(outList);
-        res.json(outList)
+        })
+        res.json(result)
     } catch (error) {
         console.error(error);
     }
-
 })
 
 
 // 출고버튼 클릭(출고 등록)
-router.post('/create/loading',async(req, res)=>{
+router.post('/create/loading', async (req, res) => {
 
-try {
-    console.log(req.body);
-    const outLoading = await Loading.update(
-        {
-            loading_type: 'O',
-            created_at: req.body.created_at,
-            loading_cnt : req.body.loading_cnt,
-            stock_shipping_des: req.body.stock_shipping_des,
-            loading_manager : req.body.loading_manager
-          },
-          {
-            where: {
-                loading_seq: req.body.loading_seq
-            }
+    try {
+        console.log(req.body);
+        const outLoading = await Loading.update(
+            {
+                loading_type: 'O',
+                created_at: req.body.created_at,
+                loading_cnt: req.body.loading_cnt,
+                stock_shipping_des: req.body.stock_shipping_des,
+                loading_manager: req.body.loading_manager
+            },
+            {
+                where: {
+                    loading_seq: req.body.loading_seq
+                }
 
-    })
-    console.log(outLoading);
-    res.json(outLoading)
-} catch (error) {
-    console.log(error);
-}
+            })
+        console.log(outLoading);
+        res.json(outLoading)
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 
 
 // 출고이력 페이지 - 모든 출고 리스트 조회
-router.post('/controll',async(req, res)=>{
-   
+router.post('/controll', async (req, res) => {
+
     let { id } = req.body;
     try {
         const outControllList = await User.findAll({
@@ -95,7 +90,7 @@ router.post('/controll',async(req, res)=>{
 
                         include: [{
                             model: Loading,
-                          
+
                             include: [{
                                 model: Stock,
 
