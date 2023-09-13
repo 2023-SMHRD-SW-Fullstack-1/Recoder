6602e62fefb6b123988349a98ac811c3c881b4c0
임혜주 <lakm1225@gmail.com>
2023-09-13 18:05:58 +0900

    0913_unique_name

--- react-project/src/components/OutDestination.jsx
+++ react-project/src/components/OutDestination.jsx
@@ -1,16 +1,30 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import ReactApexChart from 'react-apexcharts'
+// import ReactApexChart from 'react-apexcharts'
+import { Doughnut } from 'react-chartjs-2';
+import {Chart, ArcElement} from 'chart.js'
+import '../css/outDes.css'
+import 'chartjs-plugin-datalabels'
+
+
+
+Chart.register(ArcElement);
+
+
 
 function OutDestination() {
 
-  const com_seq = 1004
+const com_seq = 1004
 const wh_seq = 1004
 
-  const [desDetailList, setDesDetailList] = ([]);
-  const [desData, setDesData] = useState()
-  // 배송지 디테일 정보 불러오기
+  
+  const [desData, setDesData] = useState([])
 
+// 출고품 이름 담을 변수
+  const [sName,setSname] = useState([])
+
+
+  // 출고품 리스트 정보 불러오기
   const desDetail = async () => {
     const userData = {
       com_seq: com_seq,
@@ -18,21 +32,30 @@ const wh_seq = 1004
       
     }
     try {
-      const response = await axios.post('http://localhost:8000/out/des/detail', userData)
+      const response = await axios.post('http://localhost:8000/out/des', userData)
 
       if (response.status === 200) {
-        console.log('배송지 디테일 정보 가져오기 성공');
+        console.log('출고품 정보 가져오기 성공');
 
         console.log(response.data)
 
+        setDesData(response.data)
+        
+        const stockNames = response.data.reduce((accumulator, warehouse) => {
+          warehouse.Racks.forEach((rack) => {
+            rack.Loadings.forEach((loading) => {
+              accumulator.push(loading.Stock.stock_name);
+            });
+          });
+          return accumulator;
+        }, []);
+
+   
+
+        setSname(stockNames);
 
 
-
-       // 지피티코드  
-
-      
-
-
+     
       }
     } catch (error) {
       if (error.response && error.response.status === 401) {
@@ -42,60 +65,63 @@ const wh_seq = 1004
     }
   }
 
+  // sName 배열의 중복 제거
+  const  oneSname = [...new Set(sName)]
+  console.log("중복제거?",oneSname);
+ 
 
+  // const setStockName = [...new Set(getStockName)]
+  // console.log('중복제거 됐나요',setStockName);
+
+
+  // 차트 데이터
+  const percents = [10,20,30,50]
+
+  const data = {
+    datasets: [
+      {
+        data: percents,
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+        ],
+      },
+    ],
+    labels: ['Red', 'Orange', 'Yellow', 'Green'],
+  }
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true, // 라벨 표시 활성화
+        position: 'center', // 라벨 위치 설정 (top, bottom, left, right 등)
+      },
+      datalabels: {
+        display: true, // 라벨 표시 활성화
+        color: 'white', // 라벨 텍스트 색상 설정
+        font: {
+          size: 14, // 라벨 텍스트 크기 설정
+        },
+        formatter: (value) => {
+          return value + '%'; // 라벨 텍스트 형식 지정 (예: '10%')
+        },
+      },
+    },
+  };
+ 
   
-
-  
-
-
+ 
 
   useEffect(() => {
 
     desDetail();
   }, [])
 
-  // 도넛 차트
-  const donutData = {
-    series: [50, 40, 30, 10, 0],
-    options: {
-      chart: {
-        type: 'donut',
-      },
-      legend: {
-        position: 'bottom'
-      },
-      responsive: [{
-        breakpoint: 480,
-      }],
-      plotOptions: {
-        pie: {
-          donut: {
-            labels: {
-              show: true,
-              total: {
-                showAlways: true,
-                show: true,
-                label: 'ALARM',
-                fontSize: '12px',
-                color: 'red'
-              },
-              value: {
-                fontSize: '22px',
-                show: true,
-                color: 'blue',
-              },
-            },
-          }
-        }
-      },
-      labels: ["침입", "배회", "쓰러짐", "화재", "안전모"],
-      title: {
-        text: '배송지별 통계',
-        align: 'center'
-      },
-    },
-  }
-
+  
 
 
   return (
@@ -114,26 +140,21 @@ const wh_seq = 1004
             </th>
 
           </tr>
+          <tr>
+         
+            <td></td>
+          </tr>
         </thead>
-        <tbody>
+  
+        </table>
+    
+    {/* 도넛차트 */}
+        <div id='doughnut' >
+        <Doughnut data={data} options={options}/>
+    </div>
 
-        </tbody>
-        <div id="chart">
-          <ReactApexChart
-            options={donutData.options}
-            series={donutData.series}
-            type="donut"
-            width="500"
-          />
-        </div>
-
-
-      </table>
-      <div>
-{desDetailList.map((item,index)=>(<span key={index}>{item.stock_name} {item.loading_cnt} {item.stock_shipping_des}</span>))}
-
-
-</div>
+  
+     
     </div>
   )
 }
