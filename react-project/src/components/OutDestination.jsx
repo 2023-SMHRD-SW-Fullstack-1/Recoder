3493ed5cea15b34c05cb367a9531e444bdbaf034
임혜주 <lakm1225@gmail.com>
2023-09-14 09:57:17 +0900

    out_des_index

--- react-project/src/components/OutDestination.jsx
+++ react-project/src/components/OutDestination.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-// import ReactApexChart from 'react-apexcharts'
 import { Doughnut } from 'react-chartjs-2';
-import {Chart, ArcElement} from 'chart.js'
+import { Chart, ArcElement } from 'chart.js'
 import '../css/outDes.css'
 import 'chartjs-plugin-datalabels'
 
@@ -14,22 +13,22 @@ Chart.register(ArcElement);
 
 function OutDestination() {
 
-const com_seq = 1004
-const wh_seq = 1004
+  const com_seq = 1004
+  const wh_seq = 1004
 
-  
+
   const [desData, setDesData] = useState([])
 
-// 출고품 이름 담을 변수
-  const [sName,setSname] = useState([])
+  // 출고품 이름 담을 변수
+  const [sName, setSname] = useState([])
 
 
   // 출고품 리스트 정보 불러오기
   const desDetail = async () => {
     const userData = {
       com_seq: com_seq,
-      wh_seq:wh_seq
-      
+      wh_seq: wh_seq
+
     }
     try {
       const response = await axios.post('http://localhost:8000/out/des', userData)
@@ -40,7 +39,7 @@ const wh_seq = 1004
         console.log(response.data)
 
         setDesData(response.data)
-        
+
         const stockNames = response.data.reduce((accumulator, warehouse) => {
           warehouse.Racks.forEach((rack) => {
             rack.Loadings.forEach((loading) => {
@@ -50,12 +49,12 @@ const wh_seq = 1004
           return accumulator;
         }, []);
 
-   
+
 
         setSname(stockNames);
 
 
-     
+
       }
     } catch (error) {
       if (error.response && error.response.status === 401) {
@@ -66,16 +65,25 @@ const wh_seq = 1004
   }
 
   // sName 배열의 중복 제거
-  const  oneSname = [...new Set(sName)]
-  console.log("중복제거?",oneSname);
- 
+  const oneSname = [...new Set(sName)]
+  console.log("중복제거?", oneSname);
 
-  // const setStockName = [...new Set(getStockName)]
-  // console.log('중복제거 됐나요',setStockName);
+
+  // 테이블 클릭 이벤트
+  const [rowOutTable, setRowOutTable] = useState([].fill(false));
+
+  const handleRowClick = (idx) => {
+    console.log('클릭  인덱스', idx);
+    setRowOutTable((prevRowOutTable) => {
+      const newRowOutTable = [...prevRowOutTable];
+      newRowOutTable[idx] = !newRowOutTable[idx];
+      return newRowOutTable;
+    });
+  };
 
 
   // 차트 데이터
-  const percents = [10,20,30,50]
+  const percents = [10, 20, 30, 50]
 
   const data = {
     datasets: [
@@ -112,51 +120,59 @@ const wh_seq = 1004
       },
     },
   };
- 
-  
- 
+
+
+
 
   useEffect(() => {
 
     desDetail();
   }, [])
 
-  
+
 
 
   return (
     <div className="out_table">
-      <table className="container">
-        <thead>
-          <tr>
-            <th>
-              <h1>제품ID</h1>
-            </th>
-            <th>
-              <h1>제품명</h1>
-            </th>
-            <th>
-              <h1>마지막 출고일</h1>
-            </th>
-
-          </tr>
-          <tr>
-         
-            <td></td>
-          </tr>
-        </thead>
-  
-        </table>
+    <table className="container">
+      <thead>
+        <tr>
+          <th>
+            <h1>제품ID</h1>
+          </th>
+          <th>
+            <h1>제품명</h1>
+          </th>
+          <th>
+            <h1>마지막 출고일</h1>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {oneSname.map((item, idx) => (
+          <React.Fragment key={idx}>
+            <tr   onClick={() => handleRowClick(idx)}
+                    className={rowOutTable[idx] ? 'selected' : ''}>
+              <td  className={`out_table_id ${rowOutTable[idx] ? 'open' : ''}`}>{item}</td>
+              <td>test</td>
+              <td>test</td>
+            </tr>
+            {rowOutTable[idx] && (
+              <tr>
+                <td id='out_table_fold' colSpan={3}>
+                  {/* 도넛차트 */}
+                  <div id='doughnut'>
+                    <Doughnut data={data} options={options} />
+                  </div>
+                </td>
+              </tr>
+            )}
+          </React.Fragment>
+        ))}
+      </tbody>
+    </table>
     
-    {/* 도넛차트 */}
-        <div id='doughnut' >
-        <Doughnut data={data} options={options}/>
-    </div>
-
-  
-     
-    </div>
+</div>
   )
-}
-
-export default OutDestination
+            }
+ export default OutDestination;
