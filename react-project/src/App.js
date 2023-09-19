cc95b7faab5199541a435819fdaad4f32845f09b
shinyoungkim30 <ksyq12@gmail.com>
2023-09-19 14:43:26 +0900

    레이아웃, 대시보드 1차 완성

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,14 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Sidebar from './components/Sidebar'
-import Main from './components/Main'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
 import Join from './components/Join';
 import Out_01 from './components/Out_01'
 import StockSelect from './components/StockSelect'
 import StockManage from './components/StockManage'
-import In from './components/In'
 import OutSelect from './components/OutSelect'
 import OutDestination from './components/OutDestination'
 import WareManage from './components/WareManage'
@@ -17,19 +14,16 @@ import CreateWarehouse from './components/CreateWarehouse'
 import Warehouse from './components/Warehouse'
 import Mypage from './components/Mypage';
 import './App.css'
-import axios from 'axios';
 import Logout from './components/Logout';
 import Testcom from './components/Testcom';
 import Out_02 from './components/Out_02';
 import RegisterCompany from './components/RegisterCompany';
 import Layout from './components/Layout'
-import Table_HJ from './components/Table_HJ';
-
 import Barcode from './components/Barcode';
-import In_01 from './components/In_01';
 import Nav_HJ from './components/Nav_HJ';
 import In_HJ from './components/In_HJ';
 import Dash_HJ from './components/Dash_HJ';
+import Dashboard from './components/Dashboard'
 
 const App = () => {
 
@@ -63,31 +57,29 @@ const App = () => {
     <div>
       { uid ? (
         <div>
-          {/* <Sidebar /> */}
           <Routes>
-            <Route path='/' element={ <Login /> } />
-            <Route path='/join' element={ <Join /> } />
-            <Route path='/register/company' element={ <RegisterCompany /> } />
-            <Route path='/main' element={ <Layout /> } />
-            <Route path='/out/create' element={ <Out_01 /> } />
-            <Route path='/out/select' element={ <OutSelect /> } />
-            <Route path='/out/des' element={ <OutDestination /> } />
-            {/* <Route path='/in/create' element={ <In inputItem={inputItem}/> } /> */}
-            <Route path='/stock/select' element={ <StockSelect />} />
-            <Route path='/stock/manage' element={ <StockManage /> } />
-            <Route path='/ware/manage' element={ <WareManage comSeq={comSeq}/> } />
-            <Route path='/ware/create' element={ <WareCreate comSeq={comSeq} setNewWareData={setNewWareData} /> } />
-            <Route path='/ware/createwarehouse' element={ <CreateWarehouse comSeq={comSeq} newWareData={newWareData} /> } />
-            <Route path='/warehouse/:wh_seq' element={ <Warehouse comSeq={comSeq} /> } />
-            <Route path='/mypage' element={ <Mypage /> } />
-            <Route path='/logout' element={ <Logout /> } />
-            <Route path='/test' element={ <Testcom /> } />
-            <Route path='/out/controll' element={ <Out_02 /> } />    
-            <Route path='/barcode' element={<Barcode inputItem = {inputItem} setInputItem={setInputItem}/>}  />      
-            {/* <Route path='/in/create' element={ <In_01 inputItem={inputItem} setInputItem={setInputItem}/> } /> */}
-            <Route path = '/dash/nav' element={<Nav_HJ/>}/>
-            <Route path='/in/create' element={ <In_HJ inputItem={inputItem} setInputItem={setInputItem}/> } /> 
-            <Route path='/dash' element={ <Dash_HJ /> } /> 
+            <Route element={ <Layout /> } >
+              {/* 개발 끝나면 지워주세요 */}
+              <Route index element={ <Login /> } />
+              <Route path='/join' element={ <Join /> } />
+              <Route path='/register/company' element={ <RegisterCompany /> } />
+              {/* 개발 끝나면 지워주세요 */}
+              <Route path='/main' element={ <Dashboard /> } />
+              <Route path='/out/create' element={ <Out_01 /> } />
+              <Route path='/out/select' element={ <OutSelect /> } />
+              <Route path='/out/des' element={ <OutDestination /> } />
+              <Route path='/stock/select' element={ <StockSelect />} />
+              <Route path='/stock/manage' element={ <StockManage /> } />
+              <Route path='/ware/manage' element={ <WareManage comSeq={comSeq}/> } />
+              <Route path='/ware/create' element={ <WareCreate comSeq={comSeq} setNewWareData={setNewWareData} /> } />
+              <Route path='/ware/createwarehouse' element={ <CreateWarehouse comSeq={comSeq} newWareData={newWareData} /> } />
+              <Route path='/warehouse/:wh_seq' element={ <Warehouse comSeq={comSeq} /> } />
+              <Route path='/mypage' element={ <Mypage /> } />
+              <Route path='/logout' element={ <Logout /> } />
+              <Route path='/out/controll' element={ <Out_02 /> } />    
+              <Route path='/barcode' element={<Barcode inputItem = {inputItem} setInputItem={setInputItem}/>}  />      
+              <Route path='/in/create' element={ <In_HJ inputItem={inputItem} setInputItem={setInputItem}/> } /> 
+            </Route>          
           </Routes> 
         </div>
       ) : (
--- react-project/src/components/Layout.jsx
+++ react-project/src/components/Layout.jsx
@@ -1,28 +1,14 @@
 import React from 'react'
 import '../css/layout.css'
 import NewSidebar from './NewSidebar'
-// import Table from './Table_HJ'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
+
   return (
     <div className='layout-container'>
       <NewSidebar />
-      <div className='dashboard'>
-        <div className='dashboard-header'>헤더입니다.</div>
-        <div className='dashboard-body1'>
-          <div className='dashboard-item1'>
-            <div>
-              <p>재고 현황</p>
-            </div>
-          </div>
-          <div className='dashboard-item2'>아이템2</div>
-        </div>
-        <div className='dashboard-body2'>
-          <div className='dashboard-item3'>아이템3</div>
-          <div className='dashboard-item4'>아이템4</div>
-        </div>
-      </div>
-      
+      <Outlet />
     </div>
   )
 }
