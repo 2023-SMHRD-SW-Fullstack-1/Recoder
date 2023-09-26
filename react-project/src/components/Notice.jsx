3501c99d57aed58fe81c9523519c1a32793348c9
lim-mil <lakm1225@gmail.com>
2023-09-26 16:49:16 +0900

    notice

---
+++ react-project/src/components/Notice.jsx
@@ -0,0 +1,182 @@
+import React, { useEffect, useState } from "react";
+import Table_HJ from "./Table_HJ";
+import axios from "axios";
+import { useNavigate } from "react-router";
+import "../css/in01.css";
+import TopBoard from "./Out/TopBoard";
+import '../css/notice.css'
+import { Button, Modal } from 'antd';
+function Notice() {
+  const id = "smart";
+  const wh_seq = 1;
+  const com_seq = 1;
+const user_seq = 1
+
+// 모달 
+
+const [isModalOpen, setIsModalOpen] = useState(false);
+
+const handleOk = () => {
+  setIsModalOpen(false);
+};
+
+const handleCancel = () => {
+  setIsModalOpen(false);
+};
+
+// 재고량 수정 버튼
+const changeCnt = (record)=>{
+  setIsModalOpen(true)
+
+  console.log('크크킄클릭',record.stock_name);
+}
+
+  // 재고 데이터 조회
+  const [cntData,setCntData] = useState([])
+
+  // 알람내용 조회
+  const [alarm,setAlarm] = useState([])
+
+  const getNameList =  () => {
+    const userData = {
+      com_seq: com_seq,
+      wh_seq: wh_seq,
+      user_seq : user_seq
+    };
+
+    axios
+    .all([
+      axios.post("http://localhost:8000/notice/create", userData),
+      axios.post("http://localhost:8000/notice/alarm", userData),
+    ])
+    .then(
+      axios.spread((res1, res2) => {
+        console.log(res1.data);
+        console.log(res2.data);
+        setCntData(res1.data);
+        setAlarm(res2.data)
+
+
+      })
+
+
+    )
+    .catch((err) => {
+      console.error(err);
+    });
+   
+
+ 
+};
+
+
+
+
+const flatAlarm = alarm.map(item=>({
+  notice_content : item.notice_content,
+  stock_name:item.Stock.stock_name,
+  notice_seq:item.notice_seq
+}))
+
+
+  const columns = [
+    {
+      title: "제품명",
+      dataIndex: "stock_name",
+      key: "stock_name",
+      render: (text,  idx) => (
+        <span style={{ color: "darkgray" }}>{text}</span>
+        
+      ),
+    },
+    {
+      title: "적정 재고량",
+      dataIndex: "stock_bal_cnt",
+      key: "stock_bal_cnt",
+      render: (text, data,record, idx) => {
+        const matchingItem = flatAlarm.find((item) => item.stock_name === data.stock_name);
+        if (matchingItem) {
+          return (
+            <span style={{ color: "darkgray" }}>
+              {matchingItem.notice_content}
+            </span>
+          );
+        } else {
+          return (
+            <span style={{ color: "darkgray" }}>설정 재고량이 없습니다</span>
+          );
+        }
+      },
+    },
+    
+    {
+      title: "현재 재고",
+      dataIndex: "stock_cnt",
+      key: "stock_cnt",
+      render: (text) => <span style={{ color: "darkgray" }}>{text}</span>,
+    },
+    {
+      title: "수정",
+      dataIndex: "stock_cnt",
+      key: "stock_cnt",
+      
+      render: (text, record) => <button   onClick={() => changeCnt(record)}
+      style={{
+        color: "black",
+        backgroundColor: "white",
+        width: 60,
+        fontSize: 13,
+        height: 32,
+        paddingRight: 14,
+        paddingLeft: 14,
+        borderRadius: 6,
+        borderColor: "darkgray",
+      }}>수정</button>,
+    },
+  ];
+
+
+
+  const data = cntData.map((item, idx) => ({
+    key: idx + 1,
+    stock_name: item.Stock.stock_name,
+    stock_cnt: item.total_loading_cnt
+  }));
+
+    
+useEffect(()=>{
+  getNameList()
+ 
+},[])
+
+
+
+  return (
+    <div>
+      <div id='notice_top'>
+        <div>재고 확인</div>
+        <span>제품명</span>
+        <select type='select'>
+          {cntData.map((item,idx)=>(
+              <option key={idx} value = {item.Stock.stock_name}>
+                {item.Stock.stock_name}
+                </option>
+          ))}
+          </select><br/>
+      </div>
+      <div id = 'notice_table'>
+      <Table_HJ columns={columns} data={data}/>
+      </div>
+       <Modal title="재고량 수정" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+              <div>
+                <span>수량설정</span><input style={{margin:20,width:150}} type='text'></input>
+              </div>
+
+       </Modal>
+        </div>
+  )
+}
+
+
+
+export default Notice
